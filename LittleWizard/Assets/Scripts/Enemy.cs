9faa8414a7dbb3d730cff0563c81acb8a22fdba3
Elba <elba.fdez.lpz@gmail.com>
2025-02-13 15:31:20 +0100

    Eliminado daño por contancto

--- LittleWizard/Assets/Scripts/Enemy.cs
+++ LittleWizard/Assets/Scripts/Enemy.cs
@@ -7,8 +7,8 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] private Transform player;
     [SerializeField] private RoomGenerator roomGenerator;
+    private Transform[] nearbyPoints;
     private float speed = 1.5f;
-    private float minDistance = 0.6f;
     private int lives = 3;
 
     private Animator animator;
@@ -20,6 +20,14 @@ public class Enemy : MonoBehaviour
         roomGenerator = FindObjectOfType<RoomGenerator>();
         player = GameObject.FindGameObjectWithTag("Player").transform;
 
+        // Para encontrar los NearbyPoints
+        GameObject[] points = GameObject.FindGameObjectsWithTag("NearbyPoint"); // Busca todos los puntos esta etiqueta
+        nearbyPoints = new Transform[points.Length];    // Crea un array de Transforms
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            nearbyPoints[i] = points[i].transform; // Guarda solo el Transform
+        }
     }
 
     void Update()
@@ -32,7 +40,7 @@ public class Enemy : MonoBehaviour
             Destroy(gameObject);
         }
     }
-
+    
     void OnTriggerEnter2D(Collider2D other)
         {
             if (other.gameObject.tag == "MagicBall")
@@ -43,14 +51,14 @@ public class Enemy : MonoBehaviour
 
     private void Follow()
     {
+        Transform nearestPoint = NearbyPoint(nearbyPoints); // Encuentra el punto más cercano
         
-        // minDistance = player.position.x - 0.5f;
 
-        if (Vector2.Distance(transform.position, player.position) > minDistance)
-        {            
-            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        if (Vector2.Distance(transform.position, nearestPoint.position) > 0f){         
 
-            moveDirection = player.position - transform.position;
+            transform.position = Vector2.MoveTowards(transform.position, nearestPoint.position, speed * Time.deltaTime);
+
+            moveDirection = nearestPoint.position - transform.position;
             animator.SetFloat("Horizontal", moveDirection.x);
             animator.SetFloat("Vertical", moveDirection.y);
             animator.SetBool("IsMoving", true);
@@ -60,6 +68,25 @@ public class Enemy : MonoBehaviour
             animator.SetBool("IsMoving", false);
             Attack();
         }
+    }
+
+    Transform NearbyPoint(Transform[] nearbyPoints)     // Método para encontrar el punto más cercano
+    {
+        Transform nearestPoint = null;
+        float minDistanceSqr = Mathf.Infinity;
+        
+        foreach (Transform point in nearbyPoints)
+        {
+            float distanceSqr = (point.position - transform.position).sqrMagnitude; // Usamos sqrMagnitude para optimización
+            if (distanceSqr < minDistanceSqr)
+            {
+                minDistanceSqr = distanceSqr;
+                nearestPoint = point;
+            }
+        }
+
+        return nearestPoint;
+
     }
 
     private void Attack(){
--- LittleWizard/Assets/Scripts/Player.cs
+++ LittleWizard/Assets/Scripts/Player.cs
@@ -10,16 +10,12 @@ public class Player : MonoBehaviour
     private Rigidbody2D rbplayer;
     private Vector2 moveImput;
     private Vector2 lastMoveDirection = Vector2.down;
-    private Coroutine damageCoroutine;
 
-    // Start is called before the first frame update
     void Start()
     {
         rbplayer = GetComponent<Rigidbody2D>();
         playeranimator = GetComponent<Animator>();
     }
-
-    // Update is called once per frame
     void Update()
     {
         PlayerMovement();
@@ -33,11 +29,12 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rbplayer.MovePosition(rbplayer.position + moveImput * speed * Time.fixedDeltaTime);
+        rbplayer.MovePosition(rbplayer.position + moveImput * speed * Time.fixedDeltaTime); // Movimiento del personaje
     }
 
     private void PlayerMovement()
     {
+        // Movimiento 
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
         moveImput = new Vector2(moveX, moveY).normalized;
@@ -56,38 +53,5 @@ public class Player : MonoBehaviour
         // Guardar la última dirección en la que se movió para el idle
         playeranimator.SetFloat("LastMoveX", lastMoveDirection.x);
         playeranimator.SetFloat("LastMoveY", lastMoveDirection.y);
-    }
-
-    void OnCollisionEnter2D(Collision2D collision)  //Si lo llegara a tocar (no cuando ataca)
-    {
-        if (collision.gameObject.tag == "Enemy")
-        {
-            if (damageCoroutine == null)
-            {
-                damageCoroutine = StartCoroutine(DamageOverTime());
-            }
-        }
-    }
-
-    void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Enemy")
-        {
-            if (damageCoroutine != null)
-            {
-                StopCoroutine(damageCoroutine);
-                damageCoroutine = null;
-            }
-        }
-    }
-
-    private IEnumerator DamageOverTime()
-    {
-        while (true)
-        {
-            lives--;
-            Debug.Log("Vidas: " + lives);
-            yield return new WaitForSeconds(1f); // Ajusta el tiempo según sea necesario
-        }
     }
 }
