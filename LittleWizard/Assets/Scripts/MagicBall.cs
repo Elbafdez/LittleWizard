946921e6d222f03e62e7178201701401cd74fab7
Elba <elba.fdez.lpz@gmail.com>
2025-02-13 17:17:42 +0100

    Rotacion y animacion de la bola de magia

--- LittleWizard/Assets/Scripts/MagicBall.cs
+++ LittleWizard/Assets/Scripts/MagicBall.cs
@@ -4,9 +4,24 @@ using UnityEngine;
 
 public class MagicBall : MonoBehaviour
 {
+    public float growthRate = 0.5f; // Tasa de crecimiento por segundo
+    public float maxSize = 1f; // Tamaño máximo de la bola
+    public float rotationSpeed = 100f; // Velocidad de rotación en grados por segundo
+
     void Start()
     {
+        transform.localScale = new Vector3 (0.6f, 0.6f, 0.6f); // Inicializa la bola con tamaño cero
         Destroy(gameObject, 2f); // Destruye la bala después de 2 segundos
+    }
+
+    void Update()
+    {
+        if (transform.localScale.x < maxSize)
+        {
+            // Aumenta el tamaño de la bola gradualmente
+            transform.localScale += Vector3.one * growthRate * Time.deltaTime;
+        }
+        transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime); // Rota la bola
     }
 
     void OnCollisionEnter2D(Collision2D collision)
