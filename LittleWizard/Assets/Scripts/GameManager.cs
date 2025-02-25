604ff3eb701229e1d6f9cc51d30421a725df64e3
Elba <elba.fdez.lpz@gmail.com>
2025-02-25 12:57:41 +0100

    Space to replay

--- LittleWizard/Assets/Scripts/GameManager.cs
+++ LittleWizard/Assets/Scripts/GameManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     private int playerLives = 6;
+    private SceneManager sceneManager;
+    public string Game;
     [SerializeField] private GameObject player;
     [SerializeField] private GameObject gameOver;
     [SerializeField] public GameObject[] hearts;
@@ -16,6 +19,12 @@ public class GameManager : MonoBehaviour
             Destroy(player);
             Time.timeScale = 0;
             gameOver.SetActive(true);
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Time.timeScale = 1;
+                SceneManager.LoadScene(Game);
+            }
         }
     }
 
